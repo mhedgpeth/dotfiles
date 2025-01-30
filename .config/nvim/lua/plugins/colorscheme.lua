795994816444d01dfe398ba6fd633292fd1f17ae
Michael Hedgpeth <michael@hedge-ops.com>
2025-01-29 19:19:32 -0700

    More development today

---
+++ .config/nvim/lua/plugins/colorscheme.lua
@@ -0,0 +1,51 @@
+return {
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      colorscheme = "catppuccin",
+    },
+  },
+  {
+    "catppuccin/nvim",
+    name = "catppuccin",
+    priority = 1000,
+    lazy = false,
+    opts = {
+      -- Catppuccin comes in four flavors: mocha (dark), macchiato (darker), frappe (light-dark), and latte (light)
+      flavour = "macchiato",
+      integrations = {
+        treesitter = true,
+        semantic_tokens = true,
+        native_lsp = {
+          enabled = true,
+          virtual_text = {
+            errors = { "italic" },
+            hints = { "italic" },
+            warnings = { "italic" },
+            information = { "italic" },
+          },
+          underlines = {
+            errors = { "underline" },
+            hints = { "underline" },
+            warnings = { "underline" },
+            information = { "underline" },
+          },
+        },
+      },
+      styles = {
+        comments = { "italic" },
+        keywords = { "italic" },
+        functions = {},
+        variables = {},
+      },
+    },
+  },
+  {
+    "nvim-lualine/lualine.nvim",
+    opts = {
+      options = {
+        theme = "catppuccin",
+      },
+    },
+  },
+}

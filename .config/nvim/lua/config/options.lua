fa7842abe4aae5c5df51477eb34c4d609f0a5613
Michael Hedgpeth <michael@hedge-ops.com>
2025-03-10 07:59:54 -0600

    remove bacon-ls for now for fear it was getting in the way

--- .config/nvim/lua/config/options.lua
+++ .config/nvim/lua/config/options.lua
@@ -6,3 +6,6 @@ vim.g.lazyvim_picker = "snacks"
 -- enable overdrive scripts to be defined at the project level
 vim.o.exrc = true
 vim.o.secure = true
+
+-- turn on bacon, instructions: https://dystroy.org/bacon/community/bacon-ls/#neovim-lazyvim
+-- vim.g.lazyvim_rust_diagnostics = "bacon-ls"

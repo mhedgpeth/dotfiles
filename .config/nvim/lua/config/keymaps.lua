048dd5382f25b21d7cd6dc8044cbc5124809f6a9
Michael Hedgpeth <michael@hedge-ops.com>
2025-03-14 04:12:06 -0600

    removed autosave, updated nvim

--- .config/nvim/lua/config/keymaps.lua
+++ .config/nvim/lua/config/keymaps.lua
@@ -5,3 +5,5 @@ vim.keymap.set("n", "<leader>bo", Snacks.bufdelete.other, { desc = "Close Other 
 vim.keymap.set("n", "<leader><CR>", "<cmd>%bd<cr>", { desc = "Close All Buffers" })
 
 vim.keymap.set("n", "<leader>os", "<cmd>OverseerToggle<cr>", { desc = "Toggle Overseer" })
+
+vim.keymap.set("n", "<leader>w", ":w<CR>", { desc = "Save file" })

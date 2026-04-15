b71340cacf2581a2c60b40184dd742ad3f51c895
Michael Hedgpeth <michael@hedge-ops.com>
2026-04-15 13:17:27 -0600

    extend for leptos usage

--- home/dot_config/nvim/lua/plugins/extend-rustaceanvim.lua
+++ home/dot_config/nvim/lua/plugins/extend-rustaceanvim.lua
@@ -2,4 +2,23 @@ return {
   "mrcjkb/rustaceanvim",
   version = "^6", -- Until some bugs are figured out
   lazy = false,
+  ---@type RustaceanOpts
+  opts = {
+    server = {
+      default_settings = {
+        ["rust-analyzer"] = {
+          procMacro = {
+            ignored = {
+              leptos_macro = {
+                "server",
+              },
+            },
+          },
+          cargo = {
+            features = "all",
+          },
+        },
+      },
+    },
+  },
 }
